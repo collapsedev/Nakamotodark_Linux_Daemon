fe7bdca4543e92fd8ae0f4d7c1f2a2f705c4dd94
collapsedev <4467138+collapsedev@users.noreply.github.com>
2018-05-24 18:57:20 +0200

    Enable IRC

--- src/init.cpp
+++ src/init.cpp
@@ -388,9 +388,9 @@ bool AppInit2()
 
     fTestNet = GetBoolArg("-testnet");
     //fTestNet = true;
-    if (fTestNet) {
+    //if (fTestNet) {
         SoftSetBoolArg("-irc", true);
-    }
+    //}
 
     if (mapArgs.count("-bind")) {
         // when specifying an explicit binding address, you want to listen on it
